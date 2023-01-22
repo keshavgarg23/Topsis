5fd22ff116283c0769aff8540bb448e06ba3b3dc
keshavgarg23 <73791629+keshavgarg23@users.noreply.github.com>
2023-01-22 14:11:35 +0530

    updated settings.py

--- topsis/settings.py
+++ topsis/settings.py
@@ -123,8 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'topsis064@gmail.com'
-EMAIL_HOST_PASSWORD = 'wvqdruwiieacfjyo'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
